b6fbeb288cb81283b1952189b3fe0f56230723a4
Evgeny <capevery@gmail.com>
2021-02-08 14:34:36 +0300

    home 3 fix parallax

--- app/src/main/java/com/geekbrains/moviesearch/ui/DetailsFragment.kt
+++ app/src/main/java/com/geekbrains/moviesearch/ui/DetailsFragment.kt
@@ -2,8 +2,14 @@ package com.geekbrains.moviesearch.ui
 
 import android.os.Bundle
 import android.view.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.NestedScrollView
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import com.geekbrains.moviesearch.R
+import kotlinx.android.synthetic.main.fragment_details.*
+
 
 class DetailsFragment : Fragment() {
 
@@ -12,7 +18,26 @@ class DetailsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_details, container, false)
+        val view = inflater.inflate(R.layout.fragment_details, container, false)
+        setHasOptionsMenu(true)
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        toolbar_details.setNavigationOnClickListener { v ->
+            v.findNavController().navigateUp()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        (activity as AppCompatActivity?)?.supportActionBar?.hide()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        (activity as AppCompatActivity?)?.supportActionBar?.show()
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
